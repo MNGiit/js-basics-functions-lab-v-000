6f61fd28733d82c89124cab6a7f6d95aab72eb54
Mohammed Noori <kmanoorib@gmail.com>
2019-02-19 03:41:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,9 +22,21 @@ function distanceFromHqInFeet(distance) {
 }
 
 
+function distanceTravelledInFeet(distance) {
+  
+}
 
 
 
 
 
-
+distanceTravelledInFeet()
+returns the distance travelled in feet ‣
+ReferenceError: distanceTravelledInFeet is not defined
+    at Context.<anonymous> (test/indexTest.js:34:7)expect(distanceTravelledInFeet(43, 48)).to.equal(1320);
+returns a distance in feet ‣
+ReferenceError: distanceTravelledInFeet is not defined
+    at Context.<anonymous> (test/indexTest.js:38:7)expect(distanceTravelledInFeet(50, 60)).to.equal(2640);
+returns distance when destination is below distance ‣
+ReferenceError: distanceTravelledInFeet is not defined
+    at Context.<anonymous> (test/indexTest.js:42:7)expect(distanceTravelledInFeet(34, 28)).to.equal(1584);
