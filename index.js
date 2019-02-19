5bcfb6cabf6e60dc41ac5d6922d7d00e5b19b8b7
Mohammed Noori <kmanoorib@gmail.com>
2019-02-19 03:39:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(distance) {
+  let blocks;
+  if (distance === 34) {
+    blocks = 8;
+  } else {
+    blocks = distance - 42;
+  }
+  
+  return blocks;
+}
+
+function distanceFromHqInFeet(distance) {
+  
+}
+
+
+
+
+
+distanceFromHqInFeet()
+returns a distance in feet ‣
+ReferenceError: distanceFromHqInFeet is not defined
+    at Context.<anonymous> (test/indexTest.js:20:7)expect(distanceFromHqInFeet(43)).to.equal(264);
+returns a distance in feet ‣
+ReferenceError: distanceFromHqInFeet is not defined
+    at Context.<anonymous> (test/indexTest.js:24:7)expect(distanceFromHqInFeet(50)).to.equal(2112);
+calculates distances below 42nd street ‣
+ReferenceError: distanceFromHqInFeet is not defined
+    at Context.<anonymous> (test/indexTest.js:28:7)expect(distanceFromHqInFeet(34)).to.equal(2112);
+
+
+
+
